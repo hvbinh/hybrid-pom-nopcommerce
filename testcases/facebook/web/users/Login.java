f6a56aa369d4a4f689a7f78b5eea9761530efa0a
hvbinh <havanbinhk13t01@gmail.com>
2020-10-29 00:11:14 +0700

    Init project

---
+++ testcases/facebook/web/users/Login.java
@@ -0,0 +1,33 @@
+package facebook.web.users;
+
+import org.testng.annotations.Test;
+
+public class Login {
+	@Test
+	public void Login_01_InvalidEmail()
+	{
+		
+	}
+	@Test
+	public void Login_02_IncorrectEmail()
+	{
+		
+	}
+	@Test
+	public void Login_03_InvalidPassword()
+	{
+		
+	}
+	@Test
+	public void Login_04_IncorrectPassword()
+	{
+		
+	}
+	@Test
+	public void Login_05_ValidEmailPassword()
+	{
+		
+	}
+
+
+}
