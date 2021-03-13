14f90a6bab9d8e1501bf1ec58aa27bd005e0efd3
Binh Ha <61079709+hvbinh@users.noreply.github.com>
2021-03-13 23:38:58 +0700

    Update Practice_07_Order.java
    update log test1

--- testcases/com/nopcommerce/users/Practice_07_Order.java
+++ testcases/com/nopcommerce/users/Practice_07_Order.java
@@ -174,7 +174,7 @@ public class Practice_07_Order extends AbstractTest {
 		showBrowserConsoleLogs(driver);
 		
 		log.info("Remove frome cart - Step 01: Verify that 'Your Shopping Cart is empty!' displays");
-		log.info("test");
+		log.info("test1");
 		verifyTrue(computerPage.shoppingCartEmptyDisplay());
 	}
 
